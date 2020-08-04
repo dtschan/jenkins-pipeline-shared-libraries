98be8445e7c8ae63137233663c70ffd26761c837
Daniel Tschan <tschan@puzzle.ch>
2020-08-04 11:57:41 +0200

    Add git-crypt command

---
+++ src/com/puzzleitc/jenkins/command/GitCryptCommand.groovy
@@ -0,0 +1,32 @@
+package com.puzzleitc.jenkins.command
+
+import com.puzzleitc.jenkins.command.context.PipelineContext
+
+class GitCryptCommand {
+
+    private final PipelineContext ctx
+    private boolean unlocked
+
+    GitCryptCommand(PipelineContext ctx) {
+        this.ctx = ctx
+        unlocked = false
+    }
+
+    void execute() {
+        def credentialsId = ctx.stepParams.getOptional('credentialsId', null) as String
+        if (credentialsId) {
+            ctx.info("-- git-crypt unlock --")
+            ctx.withCredentials([ctx.file(credentialsId: credentialsId, variable: 'GIT_CRYPT_KEYFILE')]) {
+                ctx.sh script: 'git-crypt unlock $GIT_CRYPT_KEYFILE'
+                unlocked = true
+            }
+        }
+    }
+
+    void cleanUp() {
+        if (unlocked) {
+            ctx.info("-- git-crypt lock --")
+            ctx.sh script: 'git-crypt lock'
+        }
+    }
+}
